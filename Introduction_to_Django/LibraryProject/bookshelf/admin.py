9212e26e708073025f5717fe9d31a02db588b867
abdulla2699 <abdullamohamed99p@gmail.com>
2025-02-16 04:33:41 +0200

    Adding books

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -6,4 +6,3 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     list_filter = ('author', 'publication_year')
     search_fields = ('title', 'author')
-
