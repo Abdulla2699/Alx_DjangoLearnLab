d64c7c7bbfebaabef34ccd3d78cb5664b51f57d0
Abdulla Mohamed <98246870+Abdulla2699@users.noreply.github.com>
2025-02-23 03:24:16 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -11,3 +11,23 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+from django.contrib.auth import views as auth_views
+from django.urls import path, include
+
+# Login view
+class CustomLoginView(auth_views.LoginView):
+    template_name = 'relationship_app/login.html'
+
+# Logout view
+class CustomLogoutView(auth_views.LogoutView):
+    template_name = 'relationship_app/logout.html'
+
+# Registration view - we'll need to create a custom form for this
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'relationship_app/register.html'
+    success_url = reverse_lazy('login')
