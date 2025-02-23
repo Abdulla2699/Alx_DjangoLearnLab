56b6e63849b89d98a46eed1ad9fa227efdaa22c3
Abdulla Mohamed <98246870+Abdulla2699@users.noreply.github.com>
2025-02-23 03:11:01 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.detail import DetailView  # Correct import for DetailView
+from django.views.generic.detail import DetailView
 from .models import Book, Library
 
 def list_books(request):
