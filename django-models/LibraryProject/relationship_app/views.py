ba7797d247ad3f8ce03151e681abb1c91f8a6f8a
Abdulla Mohamed <98246870+Abdulla2699@users.noreply.github.com>
2025-02-23 03:37:57 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -24,3 +24,20 @@ class RegisterView(CreateView):
     form_class = UserCreationForm
     template_name = 'relationship_app/register.html'
     success_url = reverse_lazy('login')
+
+# Login view
+def custom_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('list_books')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form': form})
+
+# Logout view
+def custom_logout(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
