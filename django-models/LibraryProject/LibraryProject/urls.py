5e786f6075640097ac4307a85c9e52ce0e6f84da
Abdulla Mohamed <98246870+Abdulla2699@users.noreply.github.com>
2025-02-23 03:14:25 +0200

    Update urls.py

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from .views import BookListView, LibraryDetailView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('books/', BookListView.as_view(), name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
+
