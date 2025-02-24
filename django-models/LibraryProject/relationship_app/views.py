980e659d3d750870c90901d55cb25afec42b2bcf
Abdulla Mohamed <98246870+Abdulla2699@users.noreply.github.com>
2025-02-24 15:39:44 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -56,3 +56,16 @@ def member_view(request):
     return render(request, 'member_view.html')
 ["relationship_app/member_view.html", "relationship_app/librarian_view.html", "relationship_app/admin_view.html"]
 ["from django.contrib.auth.decorators import permission_required", "relationship_app.can_add_book", "relationship_app.can_change_book", "relationship_app.can_delete_book"]
+from django.shortcuts import render
+from .models import Book
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
+from django.views.generic import DetailView
+from .models import Library
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
