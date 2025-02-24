a5a0b6bafaae1cd805709308d3139d84f716cda8
Abdulla Mohamed <98246870+Abdulla2699@users.noreply.github.com>
2025-02-24 15:34:34 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -31,3 +31,26 @@ def logout_view(request):
         return redirect('login')  # Redirect to login page after logout
     return render(request, 'logout.html')
 ["relationship_app/register.html"]
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html')
