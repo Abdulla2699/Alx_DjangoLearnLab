a9dca97c514ddbabdcb974ea2297d31be02c07c2
Abdulla Mohamed <98246870+Abdulla2699@users.noreply.github.com>
2025-03-02 02:50:46 +0200

    Create forms.py

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,19 @@
+<!-- advanced_features_and_security/templates/bookshelf/form_example.html -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Form Example</title>
+</head>
+<body>
+    <h1>Submit Your Information</h1>
+    <form method="post" action="{% url 'your_view_name' %}">
+        {% csrf_token %}
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name"><br><br>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email"><br><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
