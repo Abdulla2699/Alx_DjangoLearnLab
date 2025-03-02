92394fe6c788a73fb3906488ef6d30e6115ac419
Abdulla Mohamed <98246870+Abdulla2699@users.noreply.github.com>
2025-03-02 02:45:02 +0200

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+# advanced_features_and_security/views.py
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, redirect
+from .models import YourModel
+
+@permission_required('app_name.can_edit', raise_exception=True)
+def edit_model(request, pk):
+    model_instance = YourModel.objects.get(pk=pk)
+    # Your edit logic here
+    return render(request, 'edit_model.html', {'model_instance': model_instance})
+
+@permission_required('app_name.can_create', raise_exception=True)
+def create_model(request):
+    if request.method == 'POST':
+        # Your create logic here
+        return redirect('model_list')
+    return render(request, 'create_model.html')
