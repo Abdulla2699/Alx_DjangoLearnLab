d785e19c2051cfd769490863e37141c54b4adcc3
Abdulla Mohamed <98246870+Abdulla2699@users.noreply.github.com>
2025-03-09 05:14:06 +0200

    Update views.py

--- api_project/api_project/api/views.py
+++ api_project/api_project/api/views.py
@@ -1,1 +1,7 @@
 
+from rest_framework.permissions import IsAuthenticated
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
