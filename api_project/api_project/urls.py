49cda6d1589aa7300277b7af5ca02107409decfd
Abdulla Mohamed <98246870+Abdulla2699@users.noreply.github.com>
2025-03-09 05:15:13 +0200

    Update urls.py

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -43,3 +43,13 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
+# api_project/urls.py
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # Connect the api app’s URLs to the project
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+]
