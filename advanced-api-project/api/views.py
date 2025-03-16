69b0c47cc04a4cd33ea138a9e8c05ea8cbd15658
abdulla2699 <abdullamohamed99p@gmail.com>
2025-03-16 17:54:00 +0200

    added

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -38,3 +38,17 @@ class BookCreateView(generics.CreateAPIView):
         serializer.save()
 ["ListView", "DetailView", "CreateView", "UpdateView", "DeleteView"]
 ["from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated", "IsAuthenticated"]
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['title', 'author__name', 'publication_year']
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ['title', 'author__name']
+["from django_filters import rest_framework"]
